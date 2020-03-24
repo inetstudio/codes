d3bfb8b7082272721a966880d606313d025ccfbd
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-24 19:22:16 +0300

    Update to 7.0

--- entities/codes/src/Contracts/Http/Requests/Front/RedeemItemRequestContract.php
+++ entities/codes/src/Contracts/Http/Requests/Front/RedeemItemRequestContract.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace InetStudio\CodesPackage\Codes\Contracts\Http\Requests\Front;
-
-use InetStudio\AdminPanel\Base\Contracts\Http\Requests\BaseRequestContract;
-
 /**
  * Interface RedeemItemRequestContract.
  */
-interface RedeemItemRequestContract extends BaseRequestContract
+interface RedeemItemRequestContract
 {
 }
