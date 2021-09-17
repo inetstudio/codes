bbc9d210fc42b9f8956d1ed997370aa190a0f455
Konstantin Bukin <bukin@inetstudio.ru>
2021-09-17 23:51:34 +0300

    Update menu

--- entities/codes/resources/views/back/includes/navigation.blade.php
+++ entities/codes/resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.codes-package.codes.*') }}">
+<li class="{{ isActiveRoute('back.codes-package.codes.*', 'mm-active') }}">
     <a href="{{ route('back.codes-package.codes.index') }}"><i class="fa fa-key"></i> <span class="nav-label">Коды</span></a>
 </li>
